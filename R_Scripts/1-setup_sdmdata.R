8928ac9cc0cfb69d64a70fe850a46b9468c0797e
MayaraMagna <mayarammallet@gmail.com>
2021-09-21 20:34:16 -0300

    Dados rodados???

--- R_Scripts/1-setup_sdmdata.R
+++ R_Scripts/1-setup_sdmdata.R
@@ -17,7 +17,7 @@ library(raster)
 ### Importando e lendo sua planilha no ambiente R. read.csv é uma função para ler a extensão .csv. NO argumento "file" coloque o caminho relativo do arquivo .csv , no arquivo "sep" indique qual o tipo de separado dos campos (o que separa as colunas).
 
 sp_input <-
-  read.csv(file = "./dados/ocorrencias/pisp_input_setupsdmdata.csv", sep = ",")
+  read.csv(file = "./dados/ocorrencias/tawsp_input_setupsdmdata.csv", sep = ",")
 
 ## Colocando no formato exigido pelo pacote: species name separated by "_"
 sp_input$species <-
